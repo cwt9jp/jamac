988dfff15f60df6a4172324f92a1b37dbf7ae574
Alex Chen <aelxcehn@yahoo.com>
2024-10-17 22:22:02 -0400

    transitions

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
 let menuButton = document.getElementById("menu-button");
 let navUL = document.getElementById("nav-ul");
 let menuImage = document.getElementById("menu-image");
+let sigma = document.getElementById('sigma');
 
 menuButton.addEventListener("click", toggleMenu);
+sigma.addEventListener("click", bruh);
 
 function toggleMenu() {
     if (navUL.style.display === "block") {
-        navUL.style = "";
+        navUL.style.display = "";
         menuImage.src = "images/menu.svg"
         menuImage.title = "Open menu"
     }
@@ -15,4 +17,8 @@ function toggleMenu() {
         menuImage.src = "images/exit.svg"
         menuImage.title = "Close menu"
     }
+}
+
+function bruh() {
+    sigma.style.width = `${300 * Math.random()}px`
 }
