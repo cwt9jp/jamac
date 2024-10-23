c590ec2dba5e15a4326025e6c63a1ecf8ae3eb80
cwt9jp <aelxcehn@yahoo.com>
2024-10-22 22:16:43 -0400

    Update and rename script.js to default.js

---
+++ scripts/default.js
@@ -0,0 +1,18 @@
+let menuButton = document.getElementById("menu-button");
+let navUL = document.getElementById("nav-ul");
+let menuImage = document.getElementById("menu-image");
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (navUL.style.display === "block") {
+        navUL.style.display = "";
+        menuImage.src = "/images/menu.svg";
+        menuImage.title = "Open menu";
+    }
+    else {
+        navUL.style.display = "block";
+        menuImage.src = "/images/exit.svg";
+        menuImage.title = "Close menu";
+    }
+}
